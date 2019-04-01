6719221925481ac177cfbc47d6c1bf6ec491e739
Kyle Fang <zhigang1992@gmail.com>
2019-04-01 23:01:29 +0800

    Remove draft

--- src/routes/posts/2019-03-08-github-action-supercharged/post.ts
+++ src/routes/posts/2019-03-08-github-action-supercharged/post.ts
@@ -1,6 +1,6 @@
 export default {
-  title: `Draft: GitHub Actions Supercharged`,
-  tags: ['github', 'github-action'],
+  title: `GitHub Actions Supercharged`,
+  tags: ["github", "github-action"],
   spoiler: `GitHub Actions really useful, and it stands out from all the current CI/CD infrastructures. Learn how you might be able to apply it in your app.`,
-  getContent: () => import('./document.mdx'),
-}
+  getContent: () => import("./document.mdx")
+};
