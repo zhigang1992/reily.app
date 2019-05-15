98f30d63bce1166728ddd9e074c9635f11ee4924
Kyle Fang <zhigang1992@gmail.com>
2019-05-15 10:41:33 +0800

    add page

---
+++ src/routes/posts/2019-05-15-code-sharing-with-react/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `Draft: Code sharing with react`,
+  tags: ["react", "react-native-web"],
+  spoiler: `Code sharing between web and mobile`,
+  getContent: () => import("./document.mdx")
+};
