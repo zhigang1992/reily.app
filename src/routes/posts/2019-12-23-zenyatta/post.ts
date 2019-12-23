e14b66ac606984378292f64bc173bb3d53ace176
Kyle Fang <zhigang1992@gmail.com>
2019-12-23 12:10:55 +0800

    add zenyatta entry

---
+++ src/routes/posts/2019-12-23-zenyatta/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `Zenyatta - A strongly typed json service`,
+  tags: ["json", "typescript", "schema", "firebase"],
+  spoiler: `Zenyatta is a service that will allow you to create OTA update JSON service in seconds, also allows your teammates to edit the same JSON at any time.`,
+  getContent: () => import("./document.mdx")
+};
